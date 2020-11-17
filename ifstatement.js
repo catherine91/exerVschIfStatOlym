62a4567c43f267f52939899680bbfe91ec34b806
catherine91 <70351707+catherine91@users.noreply.github.com>
2020-11-16 20:44:09 -0700

    changes made by Catherine

---
+++ ifstatement.js
@@ -0,0 +1,97 @@
+if(5 > 3) {
+    MediaKeySystemAccess.out.println("5 is greater than 3");
+}
+
+if("cat" = 3) {
+    MediaKeySystemAccess.out.println("is the length of 3:");
+}
+
+if("cat" = "dog") {
+    MediaKeySystemAccess.out.println("same");
+} else {
+    MediaKeySystemAccess.out.println("not the same");
+}
+
+
+var person = {
+    name: "Bobby",
+    age: 12
+}
+
+if(person.age >=18) {
+    MediaKeySystemAccess.out.println("<theNameOfThePersonInObject> is allowed to go see the movie");
+} else {
+    MediaKeySystemAccess.out.println("theNameOfThePersonInObject> is NOT allowed to go see the movie");
+}
+
+var person = {
+    name: "Bobby",
+    age: 12
+}
+
+if(person.name[0] === "B") {
+    MediaKeySystemAccess.out.println("<theNameOfThePersonInObject> is allowed to go see the movie");
+} else {
+    MediaKeySystemAccess.out.println("theNameOfThePersonInObject> is NOT allowed to go see the movie");
+}
+
+var person = {
+    name: "Bobby",
+    age: 12
+}
+
+if(person.age >=18 && person.name[0] === "B") {
+    MediaKeySystemAccess.out.println("<theNameOfThePersonInObject> is allowed to go see the movie");
+} else {
+    MediaKeySystemAccess.out.println("theNameOfThePersonInObject> is NOT allowed to go see the movie");
+}
+
+
+if(1 = 1) {
+   MediaKeySystemAccess.out.println("strict"); 
+}
+else if (1 = 1){
+   MediaKeySystemAccess.out.println("loose" or "abstract");
+}
+else {
+   MediaKeySystemAccess.out.println("not equal at all"); 
+}
+if (1< or = 4); if (2 = 4) {
+ MediaKeySystemAccess.out.println("yes");   
+}
+
+
+varName = ("Dog")
+if(typeof variableName === "string") {
+    MediaKeySystemAccess.out.println("yes");
+}else{MediaKeySystemAccess.out.println("no");
+}
+
+if(typeof true === "boolean") {
+    MediaKeySystemAccess.out.println("yes");
+}else{MediaKeySystemAccess.out.println("no");
+}
+
+if(typeof variableName != "undefined") {
+    MediaKeySystemAccess.out.println("yes");
+}else{MediaKeySystemAccess.out.println("no");
+}
+
+if("s" > 12){
+    MediaKeySystemAccess.out.println("yes");
+}
+if ("z" < 5) {
+    MediaKeySystemAccess.out.println("yes");
+}else{MediaKeySystemAccess.out.println("no");
+}
+
+if("h" > 6) {
+    MediaKeySystemAccess.out.println("yes");
+}else{MediaKeySystemAccess.out.println("no");
+}
+
+var myNum
+if(myNum % 2 === 0) {
+    MediaKeySystemAccess.out.println("<myNum> is even");
+}else{MediaKeySystemAccess.out.println("<myNum> is odd");
+}
